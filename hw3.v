d4e3ccb72d82075f1d7ff620d7a9abc2f6f6499d
Brandon Smith <bsmith83@mail.csuchico.edu>
2016-03-13 16:19:32 -0700

    Added files via upload

---
+++ hw3.v
@@ -0,0 +1,55 @@
+//Brandon Smith, John-Mark Mamalakis
+//
+//
+
+module hw3 #(parameter DATA_WIDTH=32, MATRIX_SIZE=5) (input [MATRIX_SIZE*DATA_WIDTH-1:0] x, output [MATRIX_SIZE*DATA_WIDTH-1:0] y);
+	wire [DATA_WIDTH-1:0] x_val [MATRIX_SIZE-1:0]; // x_val[MATRIX_SIZE-1] down to x_val[0] are the DATA_WIDTH-bit values
+	reg [2*DATA_WIDTH-1:0] y_val [MATRIX_SIZE-1:0]; // Resulting y values
+	
+	reg [DATA_WIDTH - 1:0] a_val [MATRIX_SIZE - 1:0][MATRIX_SIZE-1 :0]; 	//This is the array holding the matrix values of A
+	wire [2*DATA_WIDTH-1:0] p_res [MATRIX_SIZE-1:0][MATRIX_SIZE-1 :0]; 	//This is the array of wire holding the results of 
+																								//every multiplication of array elements x and a.
+	wire [2*DATA_WIDTH-1:0] sum [MATRIX_SIZE-1 :0][MATRIX_SIZE-2:0];		//This is the array of wires holding the summation
+																								//of all of the multiplied elements.
+	
+	//This initial block fills the A matrix with all of the values based on the formula.
+	integer k,m;
+	initial begin
+		for( k = MATRIX_SIZE - 1; k >=0; k=k-1) begin
+			for( m = MATRIX_SIZE - 1; m >=0; m=m-1) begin
+				a_val[k][m]= ((-1)**(k+m)) * (k+1) * (m+1); 
+			end
+		end
+	end
+		
+	//Generate components for Matrix Multiplications y = A*x.
+	genvar g,h,i,j;
+	generate
+		//unpack input x.
+		for( g=MATRIX_SIZE-1; g>=0; g=g-1 ) begin : packing_loop_x
+			assign x_val[g] = x[(g+1)*DATA_WIDTH-1:g*DATA_WIDTH];
+		end
+		
+		//Runs for each element in y.		
+		for( i=MATRIX_SIZE-1; i>=0; i=i-1 ) begin : main
+			//Multiplies each row and column of matrices A and x for y[i].
+			for( j=MATRIX_SIZE-1; j>=0; j=j-1 ) begin : multiply_loop
+				multiplier #(.DATA_WIDTH(DATA_WIDTH)) multi(.a(a_val[i][j]), .b(x_val[j]), .p(p_res[i][j]));
+			end
+			//Calculates the sum all products for y[i].
+			for( h=MATRIX_SIZE-1; h>=1; h=h-1 ) begin :accum_loop
+				
+				if(h == MATRIX_SIZE-1) begin
+					assign sum[i][h-1] = p_res[i][h] + p_res[i][h-1];
+				end
+				else begin
+					assign sum[i][h-1] = sum[i][h] + p_res[i][h-1];
+				end
+			end
+
+			//Pack output y.
+			assign y[DATA_WIDTH*(i+1)-1:DATA_WIDTH*i] = sum[i][0][DATA_WIDTH-1:0]; //y_val[i][DATA_WIDTH-1:0];
+		end
+	endgenerate
+
+endmodule
---
+++ hw3_tb.v
@@ -0,0 +1,117 @@
+//Brandon Smith, John-Mark Mamalakis
+//Even though we know that you Professor Kredo made this file.
+//Especially this file...
+
+
+module hw3_tb ();
+	// These are here for your reference and as an example.
+	// I will not test your submissions with any other values
+	// of MATRIX_SIZE and DATA_WIDTH.
+	localparam MATRIX_SIZE = 5;
+	localparam DATA_WIDTH = 32;
+
+	// Individual components of x and y
+	reg signed [DATA_WIDTH-1:0] x_vals [MATRIX_SIZE-1:0];
+	wire signed [DATA_WIDTH-1:0] y_vals [MATRIX_SIZE-1:0];
+
+	// Count of errors for each test case
+	integer error_count;
+
+	integer i;
+
+	// DUT input and output
+	wire [MATRIX_SIZE*DATA_WIDTH-1:0] x, y;
+
+	/////////////////////////
+	// Tasks and functions
+	/////////////////////////
+	
+	// Set all x_vals to the given value val
+	task set_all_x( input [DATA_WIDTH-1:0] val );
+		integer i;
+	begin
+		for( i = MATRIX_SIZE-1; i>=0; i=i-1 )
+			x_vals[i] = val;
+	end
+	endtask
+
+	// Set all x_vals to a random value
+	task set_random_x;
+		integer i;
+	begin
+		for( i = MATRIX_SIZE-1; i>=0; i=i-1 )
+			x_vals[i] = $random;
+	end
+	endtask
+
+	// Verify the matrix multiplication results against the analytical result
+	task verify_results;
+		reg signed [2*DATA_WIDTH-1:0] expected;
+		integer r, c;
+	begin
+		#5;
+		for( r = MATRIX_SIZE-1; r>=0; r=r-1 ) begin
+
+			// Compute the analytical expected value
+			expected = 0;
+			for( c = MATRIX_SIZE-1; c>=0; c=c-1 ) begin
+				expected = expected + (-1)**(r+c)*(r+1)*(c+1) * x_vals[c];
+			end
+
+			if( y_vals[r] != expected[DATA_WIDTH-1:0] ) begin
+				error_count = error_count + 1;
+				$display("[T=%t] Incorrect result for y(%d). Expected %b and got %b.", $time, r, expected, y_vals[r]);
+			end
+			#5;
+		end
+	end
+	endtask
+
+	// Instantiate the DUT
+	hw3 #(.MATRIX_SIZE(MATRIX_SIZE), .DATA_WIDTH(DATA_WIDTH)) uut (.x(x), .y(y));
+
+	////////////////////////////////
+	// Start of testbench body
+	////////////////////////////////
+	initial begin
+		// Check A*0 = 0
+		error_count = 0;
+		set_all_x(0);
+		verify_results();
+		$display("[T=%t] %d total errors checking A*0 != 0.", $time, error_count);
+
+		// Verify each matrix entry
+		error_count = 0;
+		for( i=MATRIX_SIZE-1; i>=0; i=i-1 ) begin
+			set_all_x(0);
+			x_vals[i] = 1;
+			verify_results();
+		end
+		$display("[T=%t] %d total errors checking matrix coefficients.", $time, error_count);
+
+		// Check vector of all 1's
+		error_count = 0;
+		set_all_x(1);
+		verify_results();
+		$display("[T=%t] %d total errors checking all-1 inputs.", $time, error_count);
+
+		// Check random inputs
+		for( i=MATRIX_SIZE-1; i>=0; i=i-1) begin
+			error_count = 0;
+			set_random_x();
+			verify_results();
+			$display("[T=%t] %d total errors for random input test %d.", $time, error_count, i);
+		end
+
+		#5 $finish;
+	end
+
+	// Associate x to x_vals and y to y_vals
+	genvar i_gen;
+	generate
+		for( i_gen=MATRIX_SIZE-1; i_gen>=0; i_gen=i_gen-1 ) begin : packing_loop
+			assign x[(i_gen+1)*DATA_WIDTH-1:i_gen*DATA_WIDTH] = x_vals[i_gen];
+			assign y_vals[i_gen] = y[(i_gen+1)*DATA_WIDTH-1:i_gen*DATA_WIDTH];
+		end
+	endgenerate
+endmodule
